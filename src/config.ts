5a1c8aa950cbec688f68c868296cd5f6a69d557a
Meno Abels <meno.abels@adviser.com>
2016-09-19 18:54:54 +0200

    [skip ci]

--- src/config.ts
+++ src/config.ts
@@ -6,11 +6,12 @@ import IpStore from './ip_store';
 import * as Url  from 'url';
 import * as Http  from 'http';
 import * as Https  from 'https';
+import * as Path from 'path';
 
 export class Tunator {
   public url: string = "ws://localhost:4711/tunator";
   public myAddr: IfAddrs;
-  public static fromJson(obj: any) : Tunator {
+  public static fromJson(obj: any, cfg: Config) : Tunator {
     let ret = new Tunator();
     ret.url = obj.url || ret.url;
     if (obj.myAddr) {
@@ -21,13 +22,13 @@ export class Tunator {
 }
 
 export class HttpsOptions {
-  public static fromJson(obj: any) : Https.ServerOptions {
+  public static fromJson(obj: any, cfg: Config) : Https.ServerOptions {
     if (!obj) {
       return null;
     }
     return {
-        key: fs.readFileSync(obj.key).toString() || "",
-        cert: fs.readFileSync(obj.cert).toString() || ""
+        key: fs.readFileSync(Path.join(cfg.cfgPath, obj.key)).toString() || "",
+        cert: fs.readFileSync(Path.join(cfg.cfgPath, obj.cert)).toString() || ""
       }
   }
 }
@@ -35,10 +36,10 @@ export class HttpsOptions {
 export class Bind {
   public url : Url.Url;
   public httpsOptions: Https.ServerOptions;
-  public static fromJson(obj: any) : Bind {
+  public static fromJson(obj: any, cfg: Config) : Bind {
     let ret = new Bind();
     ret.url = Url.parse(obj.url||"ws://0.0.0.0:4711");
-    ret.httpsOptions = HttpsOptions.fromJson(obj.httpsOptions);
+    ret.httpsOptions = HttpsOptions.fromJson(obj.httpsOptions, cfg);
     return ret;
   }
 }
@@ -49,14 +50,14 @@ export class Server {
   public tunator: Tunator;
   public ipStore: IpStore;
   public binds: Bind[] = [];
-  public static fromJson(obj: any) : Server {
+  public static fromJson(obj: any, cfg: Config) : Server {
     let ret = new Server();
     ret.port = obj.port || ret.port;
-    ret.tunator = Tunator.fromJson(obj.tunator||{});
+    ret.tunator = Tunator.fromJson(obj.tunator||{}, cfg);
     ret.ipStore = IpStore.fromJson(obj.ipStore||{});
     for (let bind of (obj.binds||[{url: "ws://0.0.0.0:4711"}])) {
       // console.log(">>>>", bind);
-      ret.binds.push(Bind.fromJson(bind));
+      ret.binds.push(Bind.fromJson(bind, cfg));
     }
     return ret;
   }
@@ -66,11 +67,11 @@ export class Client {
   public url: Url.Url;
   public httpsOptions: Https.ServerOptions;
   public tunator: Tunator;
-  public static fromJson(obj: any) : Client {
+  public static fromJson(obj: any, cfg: Config) : Client {
     let ret = new Client();
     ret.url = Url.parse(obj.url||"ws://localhost:4711");
-    ret.tunator = Tunator.fromJson(obj.tunator||{});
-    ret.httpsOptions = HttpsOptions.fromJson(obj.httpsOptions);
+    ret.tunator = Tunator.fromJson(obj.tunator||{}, cfg);
+    ret.httpsOptions = HttpsOptions.fromJson(obj.httpsOptions, cfg);
     return ret;
   }
 }
@@ -79,12 +80,17 @@ export class Client {
 export class Config {
   public server: Server;
   public client: Client;
+  public cfgPath: string;
 
- public static readFromString(str: string) : Config {
-    let ret = new Config();
+  constructor(cfgPath: string) {
+    this.cfgPath = cfgPath;
+  }
+
+ public static readFromString(str: string, cfgPath: string) : Config {
+    let ret = new Config(cfgPath);
     let obj = JSON.parse(str);
-    ret.server = Server.fromJson(obj.server||{});
-    ret.client = Client.fromJson(obj.client||{});
+    ret.server = Server.fromJson(obj.server||{}, ret);
+    ret.client = Client.fromJson(obj.client||{}, ret);
     return ret;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,17 @@ import PoscoClient from './posco_client'
 import PoscoContext from './posco_context'
 import Config from './config'
 import * as Fs from 'fs'
+import * as Path from 'path'
 
 console.log("Starting Posco Ecosystem");
 let context = new PoscoContext();
 let lastArg = process.argv[process.argv.length-1];
-let fs : Buffer = null
+let fname : string = "posco.json";
 if (lastArg.substr(-".json".length) == ".json") {
-    fs = Fs.readFileSync(lastArg);
+    fname = lastArg;
 }
-if (!fs) {
-    fs = Fs.readFileSync("posco.json");
-}
-context.config = Config.readFromString(fs.toString());
+let fs = Fs.readFileSync(fname);
+context.config = Config.readFromString(fs.toString(), Path.dirname(fname));
 if (process.argv.find((str)=> str.indexOf("client")!=-1)) {
     PoscoClient.main(context);
 } else {
