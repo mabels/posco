f75858dbfc03430d4851cadf59d1312d0a7681d8
Meno Abels <meno.abels@adviser.com>
2017-01-16 13:39:34 +0100

    * activated the taste writer

--- cluster/sni_proxy_service.rb
+++ cluster/sni_proxy_service.rb
@@ -28,6 +28,11 @@ module SniProxy
       #mynetworks = #{iface.address.first_ipv4.network.to_string} #{iface.address.first_ipv6 && iface.address.first_ipv6.network.to_string}
       #MAINCF
     end
+    def post_interfaces
+        up_downer = @context.find_instances_from_type(Construqt::Flavour::Nixian::Services::UpDowner::OncePerHost)
+        up_downer.add(@host, Taste::Entity.new())
+    end
+
   end
 
   module Taste
@@ -61,32 +66,14 @@ module SniProxy
           .add(Construqt::Flavour::Nixian::Tastes::File::Factory, Taste::File))
     end
 
+    def add_component(cps)
+      cps.register(SniProxy::Service).add('sniproxy')
+    end
+
     def produce(host, srv_inst, ret)
       Action.new(host, srv_inst)
     end
+
   end
 end
 
-#        def interfaces(host, ifname, iface, writer, family = nil)
-#          return unless iface.address
-#          puts "#{@service.name} #{host.name} #{ifname}"
-#          host.result.add(SniProxy::Service,
-#              Construqt::Util.render(binding, "sniproxy.conf.erb"),
-#              Construqt::Resources::Rights.root_0644(Construqt::Resources::Component::UNREF),
-#              "/etc", "sniproxy.conf")
-#          ## #{@service.get_server_iface.host.name} #{@service.get_server_iface.address.first_ipv4}
-#          #inet_protocols = all
-#          #myhostname = #{iface.host.name}
-#          #mynetworks = #{iface.address.first_ipv4.network.to_string} #{iface.address.first_ipv6 && iface.address.first_ipv6.network.to_string}
-#          #MAINCF
-#        end
-
-#      end
-
-#    end
-
-#  end
-
-#end
-
-#Construqt::Flavour::Nixian::Dialect::Ubuntu::Services.add_renderer(SniProxy::Service, SniProxy::Renderer::Nixian::Ubuntu)
